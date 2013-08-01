80ba0e3e5492c02707fb09d6a899b554dce42954
Calvin Spealman <ironfroggy@gmail.com>
2013-08-01 03:35:20 +0000

    Change wsgi settings default

--- pycarolinas_org/wsgi.py
+++ pycarolinas_org/wsgi.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pycarolinas_org.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pycarolinas_org.settings.local")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
